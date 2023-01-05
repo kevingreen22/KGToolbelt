96ec04974bce2a8f1c079d1bc84975ff6ac6dd8b
Kevin Green <stuckbykev@gmail.com>
2023-01-04 21:55:50 -0800

    Adjusted public access keyword

--- Sources/KGToolbelt/SafeArea.swift
+++ Sources/KGToolbelt/SafeArea.swift
@@ -7,7 +7,10 @@
 import SwiftUI
 
 public struct SafeArea {
-    static public var inset: UIEdgeInsets {
+    
+    public init() {}
+    
+    public static var inset: UIEdgeInsets {
         guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return .zero }
         guard let safeArea = screen.windows.first?.safeAreaInsets else { return .zero }
         return safeArea
