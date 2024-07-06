2190013a391412dd349d32912d54f54cff69fd46
Kevin Green <stuckbykev@gmail.com>
2024-07-06 15:35:49 -0700

    Added more methods for keyboard/textfields.

--- Sources/KGToolbelt/DismissKeyboard.swift
+++ Sources/KGToolbelt/DismissKeyboard.swift
@@ -1,16 +1,62 @@
 //
-//  DismissKeyboard.swift
+//  KeyboardExtensions.swift
 //
 //  Created by Kevin Green on 11/16/22.
 //
 
 import SwiftUI
-import UIKit
 
-#if canImport(UIKit)
 public extension View {
+    
+    /// Adds a done button to the top trailing edge of the keyboard for dismissal. Ideal for phone pad, number pad, or other keyboards without a return/submit key.
+    /// - Parameter isFocused: A focusState binding to toggle.
+    /// - Parameter action: An optional closure to perform after the button is tapped. Note: the button tap automatically toggles the FocusState binding.
+    @available(iOS 15.0, *)
+    func keyboardDoneButton(isFocused: FocusState<Bool>.Binding, action: (()->())? = nil) -> some View {
+        self
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocused.wrappedValue.toggle()
+                        action?()
+                    }
+                }
+            }
+            .ignoresSafeArea()
+    }
+    
+    
+    /// Dismisses the keyboard when the user taps on the screen outside of the keyboard.
+    @available(iOS 15.0, *)
+    func dismissKeyboardOnTap(_ isFocused: FocusState<Bool>.Binding) -> some View {
+        self
+            .onTapGesture {
+                isFocused.wrappedValue = false
+            }
+    }
+    
+    /// Dismisses the keyboard when the user taps on the screen outside of the keyboard.
+    @available(iOS 15.0, *)
+    func dismissKeyboardOnTap<T: Hashable>(_ focusedField: FocusState<T?>.Binding) -> some View {
+        self
+            .onTapGesture {
+                focusedField.wrappedValue = nil
+            }
+    }
+    
+    
+    /// Dismisses the keyboard when the user taps on the screen outside of the keyboard.
+    @available(iOS 15.0, *)
+    func dismissKeyboardOnTap(isFocused: FocusState<Bool>.Binding) -> some View {
+        return self
+            .onTapGesture {
+                isFocused.wrappedValue = false
+            }
+    }
+    
     func dismissKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
-#endif
+
---
+++ Sources/KGToolbelt/TextField.swift
@@ -0,0 +1,57 @@
+//
+//  LimitCharacterLength.swift
+//
+//  Created by Kevin Green on 12/2/23.
+//
+
+import SwiftUI
+import Combine
+
+extension View {
+    
+    /// Keeps text length to a limited number of characters.
+    func limitCharacterLength(limit: Int, text: Binding<String>) -> some View {
+        self
+            .onReceive(Just(text), perform: { _ in
+                if text.wrappedValue.count > limit {
+                    text.wrappedValue = String(text.wrappedValue.prefix(limit))
+                }
+            })
+    }
+    
+    /// Keeps text length to a limited number of characters.
+    func limitIntLength(limit: Int, value: Binding<Int>) -> some View {
+        self
+            .onReceive(Just(value), perform: { _ in
+                let count = String(value.wrappedValue).count
+                if count > limit {
+                    let limitedStr = String(value.wrappedValue).prefix(limit)
+                    guard let limitedInt = Int(limitedStr) else { return }
+                    value.wrappedValue = limitedInt
+                }
+            })
+    }
+    
+    /// Selects the text when this view is focused. An optional closure is available.
+    func selectAllTextOnBeginEditing(_ action: (()->())? = nil) -> some View {
+        modifier(SelectAllTextOnBeginEditingModifier(action: { action?() }))
+    }
+}
+
+
+
+
+private struct SelectAllTextOnBeginEditingModifier: ViewModifier {
+    var action: (()->())?
+    
+    public func body(content: Content) -> some View {
+        content
+            .onReceive(NotificationCenter.default.publisher(
+                for: UITextField.textDidBeginEditingNotification)) { output in
+                    DispatchQueue.main.async {
+                        UIApplication.shared.sendAction(#selector(UIResponder.selectAll(_:)), to: nil, from: nil, for: nil)
+                        action?()
+                    }
+                }
+        }
+}
